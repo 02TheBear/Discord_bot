c81e9678c28442d06e6a7685a2034eaee89ac80d
02TheBear <Fredrik.Berzins@gmail.com>
2020-03-09 23:28:31 +0100

    + command

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from auth import token
 
+from discord.ext import commands
+
 client = commands.Bot(command_prefix="§")
 
 
@@ -10,25 +12,26 @@ async def on_ready():
     print("Ready to go")
 
 
-@client.event
-async def on_message(message):
-    #    await client.send(message.channel, "hi")
-    pass
+# @client.event
+# async def on_message(message):
+#    if str(message.author) != "Lester.py#3460":
+#        print(message.author)
+#        await message.channel.send("Hi!")
 
 
 @client.event
-async def on_member_join(member):
-    print(f"hi {member}")
+async def on_guild_join(member):
+    print(f"hi {member}!")
 
 
 @client.event
-async def on_member_remove(member):
-    print(f"bye {member}")
+async def on_guild_remove(member):
+    print(f"bye {member}!")
 
 
 @client.command()
 async def ping(ctx):
-    await ctx.send("Pong!")
+    await ctx.send(f"Pong! {}")
 
 
 client.run(token)
