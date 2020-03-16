bf70e3793075e5e58722d0117aaae0d23ac3855a
oskarblo02 <svartaormen@gmail.com>
2020-03-16 09:40:26 +0100

    hej

--- bot.py
+++ bot.py
@@ -37,6 +37,9 @@ async def send_dm(ctx, member: discord.Member, *, content):
     channel = await member.create_dm()
     await channel.send(content)
 
+@client.command()
+async def help(ctx):
+    await ctx.send(f"Hello I am Lester here is some commands you may need:\n Music Commanads:\n§play, §start, §stop, §queue, §skip\n Weather Commands:\n§weather, §weather city\nTimer and Alarm Commands:\n§timer, §alarm\n Statistic Commands:\n§stats csgo, §stats rs6\n Other Commands:\n§tts, §einar, §help\nThis are some commands that will help you with my key features\nFor more information visit: https://github.com/02TheBear/Discord_bot.py/blob/master/README.md")
 
 # @client.event
 # async def on_message(message):
