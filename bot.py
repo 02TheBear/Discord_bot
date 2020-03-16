3af7af4e919edd08b807650c583bb9fdb7107c71
02TheBear <Fredrik.Berzins@gmail.com>
2020-03-16 09:11:01 +0100

    - spam command

--- bot.py
+++ bot.py
@@ -11,13 +11,6 @@ async def on_ready():
     print("Ready to go")
 
 
-# @client.event
-# async def on_message(message):
-#    if str(message.author) != "Lester.py#3460":
-#        print(message.author)
-#        await message.channel.send("Hi!")
-
-
 @client.event
 async def on_guild_join(member):
     print(f"hi {member}!")
@@ -32,6 +25,24 @@ async def on_guild_remove(member):
 async def ping(ctx):
     start_time = time.time()
     await ctx.send(f"Pong!\n{(int((time.time() - start_time)/10000)*10)}")
+
+
+@client.command()
+async def hi(ctx):
+    await ctx.send(f"HI!")
+
+
+@client.command()
+async def send_dm(ctx, member: discord.Member, *, content):
+    channel = await member.create_dm()
+    await channel.send(content)
+
+
+# @client.event
+# async def on_message(message):
+#    if str(message.author) != "Lester.py#3460":
+#        print(message.author)
+#        await message.channel.send("what??")
 
 
 client.run(token)
