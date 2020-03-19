b8455be7179503f877326f91670d7be8e6cdd3c7
02TheBear <Fredrik.Berzins@gmail.com>
2020-03-19 20:31:45 +0100

    + urllib3 to requierments

--- testing.py
+++
@@ -1,8 +0,0 @@
-# from cmds.api.api_func.func_weather_api import weather
-
-# city = input("city? ")
-# countries = input("countries? ")
-# print(weather())
-
-# print(weather(input_city=city, input_countries=countries))
-
