09dcd95d89939d402db9be151d4e83893ee0e57c
02TheBear <Fredrik.Berzins@gmail.com>
2020-04-23 11:56:24 +0200

    temp fix for demo

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ import urllib.request
 import urllib.parse
 import re
 
+
 # Local imports
 from settings import prefix
 
@@ -54,6 +55,62 @@ for filename in os.listdir("./commands/func"):
 async def ping(ctx):
     start_time = time.time()
     await ctx.send(f"Pong!\n{int(client.latency * 1000)}ms")
+
+
+@client.command()
+async def playsong(ctx, song: str):
+    voice = get(client.voice_clients, guild=ctx.guild)
+    voice.play(discord.FFmpegPCMAudio("GangnamBusters.mp3"))
+    voice.volume = 20
+    voice.is_playing()
+
+
+# Music play command
+@client.command()
+async def play(ctx, url: str):
+    if ctx.message.author.voice:
+        await ctx.message.author.voice.channel.connect()
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send(
+            "Wait for the current playing music end or use the 'stop' command"
+        )
+        return
+    await ctx.send("Getting everything ready, playing audio soon")
+    voice = get(client.voice_clients, guild=ctx.guild)
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }
+        ],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        if validators.url(url):
+            pass
+        else:
+            query_string = urllib.parse.urlencode({"search_query": url})
+            html_content = urllib.request.urlopen(
+                "http://www.youtube.com/results?" + query_string
+            )
+            search_results = re.findall(
+                r"href=\"\/watch\?v=(.{11})", html_content.read().decode()
+            )
+            url = "http://www.youtube.com/watch?v=" + str(search_results[0])
+        print(url)
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    voice.volume = 20
+    voice.is_playing()
 
 
 if __name__ == "__main__":
